8ffa3b7f680acc51386e4dc47aeb53cd0883e156
BestDragon69 <quillon.florian@gmail.com>
2026-04-13 20:37:50 +0200

    Add files via upload

---
+++ 96.jsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyDPB6tjA8bmbhdMRID0BD3FiA-V2vtVccY",
+            authDomain: "jeu96-13a82.firebaseapp.com",
+            databaseURL: "https://jeu96-13a82-default-rtdb.europe-west1.firebasedatabase.app",
+            projectId: "jeu96-13a82",
+            storageBucket: "jeu96-13a82.firebasestorage.app",
+            messagingSenderId: "814796348584",
+            appId: "1:814796348584:web:9db47cc7e78bfef42c769d",
+            measurementId: "G-N903VRMGYC"
+        };
